bc206b00e8a5ea17703da89c0058642ed4504a56
Alan Matthews <matthewsa1320@gmail.com>
2015-03-03 15:02:55 -0500

    Setting up log out on views, before action authenticate on lists controller

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,5 +1,6 @@
 class TodoListsController < ApplicationController
   before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
 
   # GET /todo_lists
   # GET /todo_lists.json
@@ -63,12 +64,12 @@ class TodoListsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_todo_list
-      @todo_list = TodoList.find(params[:id])
-    end
+  def set_todo_list
+    @todo_list = TodoList.find(params[:id])
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def todo_list_params
-      params.require(:todo_list).permit(:title, :description)
-    end
+  def todo_list_params
+    params.require(:todo_list).permit(:title, :description)
+  end
 end
